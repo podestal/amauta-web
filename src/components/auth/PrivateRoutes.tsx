d5d2870cee27a77f7645e9a0fb6a29f15f494f0a
podestal <luisrodriguezpodesta@gmail.com>
2025-01-04 10:26:41 -0500

    get instructor in privateroutes component

--- src/components/auth/PrivateRoutes.tsx
+++ src/components/auth/PrivateRoutes.tsx
@@ -1,5 +1,9 @@
+import useGetInstructor from "../../hooks/api/instructor/useGetInstructor"
 import useAuthStore from "../../hooks/store/useAuthStore"
 import { Navigate } from "react-router-dom"
+import useInstructorStore from "../../hooks/store/useInstructorStore"
+import { useEffect } from "react"
+import useLoader from "../../hooks/ui/useLoader"
 
 interface Props {
     children: React.ReactElement
@@ -7,7 +11,19 @@ interface Props {
 
 const PrivateRoutes = ({ children }: Props) => {
 
-    const access = useAuthStore(s => s.access)
+    const access = useAuthStore(s => s.access) || ''
+    const {data: instructor, isLoading, isError, error, isSuccess} = useGetInstructor({ access })
+    const setInstructor = useInstructorStore(s => s.setInstructor)
+
+    useEffect(() => {
+        instructor && setInstructor(instructor)
+    }, [instructor, setInstructor])
+
+    useLoader(isLoading)
+
+    if (isError) return <p>Error: {error.message}</p>
+
+    if (isSuccess)
 
     return access ? children : <Navigate to="/login" replace />
 
--- src/pages/StudentsPage.tsx
+++ src/pages/StudentsPage.tsx
@@ -1,25 +1,6 @@
-import { useEffect } from "react"
 import Clasrooms from "../components/api/ClassRooms/Clasrooms"
-import useGetInstructor from "../hooks/api/instructor/useGetInstructor"
-import useInstructorStore from "../hooks/store/useInstructorStore"
-import useAuthStore from "../hooks/store/useAuthStore"
-import useLoader from "../hooks/ui/useLoader"
 
 const StudentsPage = () => {
-
-    const access = useAuthStore(s => s.access) || ''
-    const {data: instructor, isLoading, isError, error, isSuccess} = useGetInstructor({ access })
-    const setInstructor = useInstructorStore(s => s.setInstructor)
-
-    useEffect(() => {
-        instructor && setInstructor(instructor)
-    }, [instructor, setInstructor])
-
-    useLoader(isLoading)
-
-    if (isError) return <p>Error: {error.message}</p>
-
-    if (isSuccess)
 
   return (
     <div className="w-full max-w-[95%] sm:max-w-[600px] md:max-w-[800px] lg:max-w-[1024px] xl:max-w-[1200px] 2xl:max-w-[1280px] mx-auto overflow-hidden">
