a813a0f4b92dcc4f63110a2fc1f8e93dfb6809b3
podestal <luisrodriguezpodesta@gmail.com>
2025-01-08 10:28:11 -0500

    text-base for inputs and textareas

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -96,11 +96,11 @@ const Input = forwardRef<HTMLInputElement, Props>(({
                 <style dangerouslySetInnerHTML={{ __html: styles.animation }} />
                 <input
                     ref={ref}
-                    className={`bg-slate-100 dark:bg-gray-950 border-neutral-400 dark:border-gray-800 border-2 rounded-lg w-full dark:text-slate-50 text-xs px-2 py-2 focus:border-blue-700 focus:outline-none
+                    className={`bg-slate-100 dark:bg-gray-950 border-neutral-400 dark:border-gray-800 border-2 rounded-lg w-full dark:text-slate-50 text-xs px-2 py-2 focus:border-blue-700 focus:outline-none sm:text-base
                                 ${showError ? 'border-red-500 dark:border-red-600 shake' : 'dark:border-gray-800 border-neutral-200'}
                             `}
                     placeholder={placeholder ? placeholder : 'Input ...'}
-                    type={showPassword && type === 'password' ? 'text' : type} // Use showPassword to toggle visibility
+                    type={showPassword && type === 'password' ? 'text' : type} 
                     value={value}
                     onChange={handleInputChange}
                     {...props} 
