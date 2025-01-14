83e2aa5db6b6dde4015ff966efb7460b24487db4
podestal <luisrodriguezpodesta@gmail.com>
2025-01-14 08:04:55 -0500

    profile page

---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,40 @@
+interface Props {
+  firstName: string;
+  lastName: string;
+  size?: number; 
+}
+
+const Avatar = ({ firstName, lastName, size = 50 }: Props) => {
+
+  const initials = `${firstName[0].toUpperCase()}${lastName[0].toUpperCase()}`;
+
+  const randomGradient = () => {
+    const colors = [
+      "#FF9A8B, #FF6A88",
+      "#A18CD1, #FBC2EB",
+      "#FAD961, #F76B1C",
+      "#43E97B, #38F9D7",
+      "#667EEA, #764BA2",
+      "#30CFFD, #330867",
+    ];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return `linear-gradient(to right, ${colors[randomIndex]})`;
+  };
+
+  return (
+    <div
+      className="flex items-center justify-center text-white font-bold"
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        borderRadius: "50%",
+        background: randomGradient(),
+        fontSize: `${size / 2.5}px`,
+      }}
+    >
+      {initials}
+    </div>
+  );
+};
+
+export default Avatar;
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,11 +1,42 @@
 import Logout from "../components/auth/Logout"
-import InstallPWAButton from "../components/ui/InstallPWAButton"
+import Avatar from "../components/ui/Avatar"
+import Button from "../components/ui/Button"
+import useGetProfileStore from "../hooks/store/useGetProfileStore"
 
 const ProfilePage = () => {
+
+  const profile = useGetProfileStore(s => s.profile)
+  const user = useGetProfileStore(s => s.user)
+
   return (
     <div className="w-full max-w-[95%] sm:max-w-[600px] md:max-w-[800px] lg:max-w-[1024px] xl:max-w-[1200px] 2xl:max-w-[1280px] mx-auto overflow-hidden">
-        <Logout />
-        <InstallPWAButton />
+        <div className="p-6 bg-gray-100 dark:bg-slate-900 rounded shadow-lg max-w-md mx-auto">
+          <div className="flex flex-col items-center mb-6">
+          {profile && <Avatar 
+            firstName={profile?.first_name}
+            lastName={profile?.last_name}
+          />}
+          <h1 className="text-lg font-bold">{profile?.first_name} {profile?.last_name}</h1>
+        </div>
+        <div className="mb-6">
+          <h2 className="text-md font-semibold mb-2">Información de Cuenta</h2>
+          <p className="text-sm">
+            <strong>Email:</strong> {user?.email}
+          </p>
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <Logout />
+          <Button 
+            label="Cambiar contraseña"
+          />
+          <Button 
+            label="Cambiar correo electrónico"
+          />
+        </div>
+      </div>
+        
+        
+
     </div>
   )
 }
