25f8914d3f1594974156ed0d1a4ac127484c8f4d
podestal <luisrodriguezpodesta@gmail.com>
2025-01-23 11:59:20 -0500

    showing clases for instructors and assistants

--- src/components/api/ClassRooms/Clasrooms.tsx
+++ src/components/api/ClassRooms/Clasrooms.tsx
@@ -3,54 +3,46 @@ import ClassroomCard from "./ClassroomCard"
 import useGetProfileStore from "../../../hooks/store/useGetProfileStore"
 import { Instructor } from "../../../services/api/instructorService"
 import { Assistant } from "../../../services/api/assistantService"
+import useLanguageStore from "../../../hooks/store/useLanguageStore"
 
 const Clasrooms = () => {
 
     const user = useGetProfileStore(s => s.user)
     const profile = useGetProfileStore(s => s.profile)
-    console.log('user', user);
+    const lan = useLanguageStore(s => s.lan)
     
-    const instructor = user?.groups[0] === 'instructor' && profile as Instructor
 
-    const assistant = user?.groups[0] === 'assistant' && profile as Assistant
+    const role = user?.groups[0]
+    const isInstructor = role === 'instructor'
+    const isAssistant = role === 'assistant'
 
-    if (instructor && !instructor?.clases_details?.length) return <p>No classes available</p>;
+    const classrooms = isInstructor
+        ? (profile as Instructor)?.clases_details
+        : isAssistant
+        ? (profile as Assistant)?.clases_details
+        : []
 
-    console.log('assistant',assistant);
+    if (!classrooms?.length) return <p>{lan === 'EN' ? 'No classes available' : 'No hay clases disponibles'}</p>;
     
     
   return (
-    <>
-    {assistant && 
-    <div className="flex flex-col justify-start items-center gap-6 w-[90%] mx-auto mb-32">
-        <h2 className="text-2xl font-bold text-center">Clases</h2>
-        {assistant.clases_details.length > 0 && assistant.clases_details.map( clasroom => (
-            <ClassroomCard 
-                key={clasroom}
-                classroom={clasroom}
-            />
-        ))}
-    </div> 
-    }
-    {instructor && 
-    <div>
-        {instructor.clases_details.length > 1 ? 
+    <div className="mb-32">
+        {classrooms.length > 1 
+        ?
         <div className="flex flex-col justify-start items-center gap-6 w-[90%] mx-auto">
             <h2 className="text-2xl font-bold text-center">Clases</h2>
-            {instructor.clases_details.map( clasroom => (
+            {classrooms.map( clasroom => (
                 <ClassroomCard 
                     key={clasroom}
                     classroom={clasroom}
                 />
             ))}
         </div> 
-        : 
+    : 
         <Students 
-            classroom={instructor.clases_details[0].split('-').pop()}
-        />
-        }
-    </div>}
-    </>
+            classroom={classrooms[0].split('-').pop()}
+        />}
+    </div>
   )
 }
 
