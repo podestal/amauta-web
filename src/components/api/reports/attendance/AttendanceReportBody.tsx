99438f2d156f31bbcae58d937fa27a2d39409de9
podestal <luisrodriguezpodesta@gmail.com>
2025-01-26 09:27:57 -0500

    body and header for report

---
+++ src/components/api/reports/attendance/AttendanceReportBody.tsx
@@ -0,0 +1,94 @@
+import moment from 'moment'
+import { statusStyles } from "../../attendance/AttendanceStatus"
+import useAuthStore from '../../../../hooks/store/useAuthStore'
+import useGetStudents from '../../../../hooks/api/student/useGetStudents'
+import useLanguageStore from '../../../../hooks/store/useLanguageStore'
+
+interface Props {
+    weekDays: moment.Moment[]
+    selectedClassroom: string
+    selectedWeek: string
+}
+
+const AttendanceReportBody = ({ weekDays, selectedClassroom, selectedWeek }: Props) => {
+
+    const access = useAuthStore(s => s.access) || ''
+    const lan = useLanguageStore(s => s.lan)
+
+    const { data: students, isLoading, isError, error, isSuccess } = useGetStudents({ access, classroomId: selectedClassroom, week: selectedWeek })
+
+    if (isLoading) return <p className="text-2xl text-center my-10 animate-pulse">{lan === 'EN' ? 'Loading ...' : 'Cargando ...'}</p>
+
+    if (isError) return <p>Error {error.message}</p>
+
+    if (isSuccess && students.length > 0)
+
+  return (
+    <>
+    {students.map( student => (
+        <div 
+            key={student.uid}
+            className="w-full grid grid-cols-12 px-2 py-4 font-palanquin text-left hover:bg-slate-100 dark:hover:bg-slate-900">
+            {/* <div className="w-12 h-12 flex items-center justify-center bg-gradient-to-r from-blue-800 to-slate-900 rounded-full dark:text-slate-50 font-bold text-md overflow-hidden">
+                {student.picture ? (
+                    <img 
+                    src={student.picture || `https://ui-avatars.com/api/?name=${student.first_name}+${student.last_name}&background=random`} 
+                    alt={`${student.first_name} ${student.last_name}`} 
+                    className="w-full h-full object-cover rounded-full"
+                    />
+                ) : (
+                    <span>
+                    {student.first_name[0]}{student.last_name[0]}
+                    </span>
+                )}
+            </div> */}
+            <div className="flex justify-start items-center">
+                <p>{student.uid}</p>
+            </div>
+            <div className="flex justify-start items-center col-span-2">
+                <p>{student.first_name}</p>
+            </div>
+            <div className="flex justify-start items-center col-span-2">
+                <p>{student.last_name}</p>
+            </div>
+            <div>
+                
+            </div>
+            {weekDays.map((day, index) => {
+                const attendanceIn = student.attendances_in.find((attendance) =>
+                    moment(attendance.created_at).isSame(day, "day")
+                );
+                const attendanceOut = student.attendances_out.find((attendance) =>
+                    moment(attendance.created_at).isSame(day, "day")
+                );
+
+                return (
+                    <div key={index} className="w-full flex justify-left items-center gap-2">
+                        <div
+                            className={`w-8 h-8 flex justify-center items-center ${
+                                attendanceIn ? statusStyles[attendanceIn.status] : "bg-gray-600"
+                            } rounded-lg`}
+                        >
+                            {attendanceIn ? attendanceIn.status : "-"}
+                        </div>
+                        <div
+                            className={`w-8 h-8 flex justify-center items-center ${
+                                attendanceOut ? statusStyles[attendanceOut.status] : "bg-gray-600"
+                            } rounded-lg`}
+                        >
+                            {attendanceOut ? attendanceOut.status : "-"}
+                        </div>
+                    </div>
+                );
+            })}
+            <div>
+
+            </div>
+        </div>
+    ))}
+    </>
+  )
+  else return <p className="text-2xl text-center my-10">{lan === 'EN' ? 'No students found in this classroom' : 'No se encontraron alumnos en esta clase'}</p>
+}
+
+export default AttendanceReportBody
--- src/components/api/reports/attendance/AttendanceReportTable.tsx
+++ src/components/api/reports/attendance/AttendanceReportTable.tsx
@@ -1,10 +1,7 @@
-import { RiArrowDownSFill, RiArrowLeftCircleFill, RiArrowRightCircleFill } from "@remixicon/react"
-import useLanguageStore from "../../../../hooks/store/useLanguageStore"
-import useAuthStore from "../../../../hooks/store/useAuthStore"
-import useGetStudents from "../../../../hooks/api/student/useGetStudents"
-import { statusStyles } from "../../attendance/AttendanceStatus"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import moment from "moment"
+import AttendanceReportHeader from "./AttendanceReportHeader"
+import AttendanceReportBody from "./AttendanceReportBody"
 
 interface Props {
     selectedClassroom: string
@@ -12,15 +9,8 @@ interface Props {
 
 const AttendanceReportTable = ({ selectedClassroom }: Props) => {
 
-    const lan = useLanguageStore(s => s.lan)
-    const access = useAuthStore(s => s.access) || ''
     const [selectedWeek, setSelectedWeek] = useState(moment().week().toString())
 
-
-    useEffect(() => {
-        console.log('selectedWeek', selectedWeek);
-        
-    }, [selectedWeek])
     const getWeekDays = (week: number) => {
         const year = moment().year()
         const startOfWeek = moment().year(year).week(week).startOf('isoWeek')
@@ -28,139 +18,23 @@ const AttendanceReportTable = ({ selectedClassroom }: Props) => {
 
     }
 
-    const { data: students, isLoading, isError, error, isSuccess } = useGetStudents({ access, classroomId: selectedClassroom, week: selectedWeek })
-
-    const handleNextWeek = () => {
-        if (parseInt(selectedWeek) < 52) {
-            setSelectedWeek((parseInt(selectedWeek) + 1).toString())
-        }
-    }
-
-    const handlePrevWeek = () => {
-        if (parseInt(selectedWeek) > 1) {
-            setSelectedWeek((parseInt(selectedWeek) - 1).toString())
-        }
-    }
-
     const weekDays = getWeekDays(parseInt(selectedWeek) +1)
 
-    if (isLoading) return <p className="text-2xl text-center my-10 animate-pulse">{lan === 'EN' ? 'Loading ...' : 'Cargando ...'}</p>
-
-    if (isError) return <p>Error {error.message}</p>
-
-    if (isSuccess && students.length > 0)
 
   return (
     <>
-    <div className="w-full grid grid-cols-12 dark:bg-slate-900 bg-gray-200 font-bold px-2 py-6">
-            {/* <div className="flex py-1 text-left hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer">
-                
-            </div> */}
-            <div className="flex py-1 text-left hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer">
-                <p>{lan === 'EN' ? 'UID' : 'UID'}</p> 
-            </div>
-            <div className="flex py-1 text-left col-span-2 hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer">
-                <p>{lan === 'EN' ? 'Name' : 'Nombres'}</p>
-                <RiArrowDownSFill 
-                    // className={`${sortKey === 'category_name' && sortOrder === 'desc' ? 'rotate-180' : ''}`}
-                />
-            </div>
-            <div className="flex py-1 text-left col-span-2 hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer">
-                <p>{lan === 'EN' ? 'Lastname' : 'Apellidos'}</p>
-                <RiArrowDownSFill 
-                    // className={`${sortKey === 'created_at' && sortOrder === 'desc' ? 'rotate-180' : ''}`}
-                />
-            </div>
-            <div>
-                <RiArrowLeftCircleFill 
-                    className="text-blue-600 hover:text-blue-800 cursor-pointer"
-                    onClick={handlePrevWeek}
-                />
-            </div>
-            {weekDays.map((day, index) => (
-                <div
-                key={index}
-                className="flex py-1 text-left hover:text-slate-700 dark:hover:text-slate-300 cursor-pointer"
-            >
-                <p>
-                    {lan === "EN"
-                        ? day.format("ddd DD")
-                        : day.locale("es").format("ddd DD")} 
-                </p>
-            </div>
-            ))}
-            <div>
-                <RiArrowRightCircleFill 
-                    className="text-blue-600 hover:text-blue-800 cursor-pointer"
-                    onClick={handleNextWeek}
-                />
-            </div>
-        </div>
-        {students.map( student => (
-            <div 
-                key={student.uid}
-                className="w-full grid grid-cols-12 px-2 py-4 font-palanquin text-left hover:bg-slate-100 dark:hover:bg-slate-900">
-                {/* <div className="w-12 h-12 flex items-center justify-center bg-gradient-to-r from-blue-800 to-slate-900 rounded-full dark:text-slate-50 font-bold text-md overflow-hidden">
-                    {student.picture ? (
-                        <img 
-                        src={student.picture || `https://ui-avatars.com/api/?name=${student.first_name}+${student.last_name}&background=random`} 
-                        alt={`${student.first_name} ${student.last_name}`} 
-                        className="w-full h-full object-cover rounded-full"
-                        />
-                    ) : (
-                        <span>
-                        {student.first_name[0]}{student.last_name[0]}
-                        </span>
-                    )}
-                </div> */}
-                <div className="flex justify-start items-center">
-                    <p>{student.uid}</p>
-                </div>
-                <div className="flex justify-start items-center col-span-2">
-                    <p>{student.first_name}</p>
-                </div>
-                <div className="flex justify-start items-center col-span-2">
-                    <p>{student.last_name}</p>
-                </div>
-                <div>
-                    
-                </div>
-                {weekDays.map((day, index) => {
-                    const attendanceIn = student.attendances_in.find((attendance) =>
-                        moment(attendance.created_at).isSame(day, "day")
-                    );
-                    const attendanceOut = student.attendances_out.find((attendance) =>
-                        moment(attendance.created_at).isSame(day, "day")
-                    );
-
-                    return (
-                        <div key={index} className="w-full flex justify-left items-center gap-2">
-                            <div
-                                className={`w-8 h-8 flex justify-center items-center ${
-                                    attendanceIn ? statusStyles[attendanceIn.status] : "bg-gray-600"
-                                } rounded-lg`}
-                            >
-                                {attendanceIn ? attendanceIn.status : "-"}
-                            </div>
-                            <div
-                                className={`w-8 h-8 flex justify-center items-center ${
-                                    attendanceOut ? statusStyles[attendanceOut.status] : "bg-gray-600"
-                                } rounded-lg`}
-                            >
-                                {attendanceOut ? attendanceOut.status : "-"}
-                            </div>
-                        </div>
-                    );
-                })}
-                <div>
-
-                </div>
-            </div>
-        ))}
-        
+        <AttendanceReportHeader 
+            weekDays={weekDays}
+            selectedWeek={selectedWeek}
+            setSelectedWeek={setSelectedWeek}
+        />
+        <AttendanceReportBody 
+            weekDays={weekDays}
+            selectedClassroom={selectedClassroom}
+            selectedWeek={selectedWeek}
+        />
     </>
   )
-  else return <p className="text-2xl text-center my-10">{lan === 'EN' ? 'No students found in this classroom' : 'No se encontraron alumnos en esta clase'}</p>
 }
 
 export default AttendanceReportTable
