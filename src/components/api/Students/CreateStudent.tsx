52a76a0c219970ca1baed30137254205f647c613
podestal <luisrodriguezpodesta@gmail.com>
2025-01-27 10:17:15 -0500

    basic form for student

--- src/components/api/Students/CreateStudent.tsx
+++ src/components/api/Students/CreateStudent.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
 import StudentForm from "./StudentForm"
 
 const CreateStudent = () => {
+
+  const [page, setPage] = useState(1)
+
   return (
-    <StudentForm />
+    <div className="pt-12">
+      <h2 className="text-4xl mb-16 text-center font-bold">Ficha de Inscripción</h2>
+      {page === 1 && <StudentForm 
+        setPage={setPage}
+      />}
+      {page === 2 && <p>Page 2</p>}
+    </div>
   )
 }
 
