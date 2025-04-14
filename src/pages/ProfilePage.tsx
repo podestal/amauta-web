1c9164bf2ec9de9364330b9718e57c7da61a1d55
podestal <luisrodriguezpodesta@gmail.com>
2025-04-14 08:01:26 -0400

    removing not usable buttons in profile

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import Logout from "../components/auth/Logout";
 import Avatar from "../components/ui/Avatar";
-import { RiMailFill, RiPhoneFill, RiEdit2Fill, RiLockPasswordFill } from "@remixicon/react";
+import { RiMailFill, RiPhoneFill } from "@remixicon/react";
 import useGetProfileStore from "../hooks/store/useGetProfileStore";
 
 const ProfilePage = () => {
@@ -14,7 +14,8 @@ const ProfilePage = () => {
         {/* Avatar & Name */}
         <div className="flex flex-col items-center">
           {profile && <Avatar firstName={profile?.first_name} lastName={profile?.last_name} />}
-          <h1 className="text-xl font-bold mt-3">{profile?.first_name} {profile?.last_name}</h1>
+          <h1 className="text-xl font-bold mt-3">{profile?.first_name}</h1>
+          <h1 className="text-xl font-bold mt-3">{profile?.last_name}</h1>
           <p className="text-gray-500 dark:text-gray-400 text-sm">{user?.email}</p>
         </div>
 
@@ -32,12 +33,12 @@ const ProfilePage = () => {
 
         {/* Action Buttons */}
         <div className="w-[70%] mx-auto mt-8 flex flex-col gap-4">
-          <button className="flex items-center justify-center gap-3 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg shadow-md transition">
+          {/* <button className="flex items-center justify-center gap-3 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg shadow-md transition">
             <RiEdit2Fill className="text-lg" /> Editar Perfil
           </button>
           <button className="flex items-center justify-center gap-3 bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-lg shadow-md transition">
             <RiLockPasswordFill className="text-lg" /> Cambiar Contraseña
-          </button>
+          </button> */}
           <Logout />
         </div>
       </div>
