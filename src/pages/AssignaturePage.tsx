50c8b8b27ef86fc39b98738bdc61148177504044
podestal <luisrodriguezpodesta@gmail.com>
2025-04-20 07:58:57 -0400

    connection to gemini done

--- src/pages/AssignaturePage.tsx
+++ src/pages/AssignaturePage.tsx
@@ -1,19 +1,37 @@
-// import { useLocation } from 'react-router-dom'
-import Activities from '../components/api/activity/Activities'
+// import Activities from '../components/api/activity/Activities'
+import { GoogleGenAI } from '@google/genai'
+import { useState } from 'react'
+import Input from '../components/ui/Input'
 
 const AssignaturePage = () => {
 
-    // const assignatureId = useLocation().pathname.split('/')[3]
-    // const area = useLocation().state.area
+  const [topic, setTopic] = useState('')
+  const apiKey = import.meta.env.VITE_GEMINI_KEY
+  const googleGenAI = new GoogleGenAI({
+    apiKey: apiKey,})
     
+  const getAIResponse = async (topic: string) => {
+    const response = await googleGenAI.models.generateContent({
+      model: 'gemini-2.0-flash',
+      contents: `Make a lesson for a 5th grade class about this topic ${topic}, this lesson is going to be 50 minutes long, and it has to be very fun and interactive. after the lesson give me some ways to interact with the students in order to them to learn this . The lesson has to be in Spanish. just add the lesson, do not say sure here you have, or at the end do not say I hope this works, just the lesson period`,
+    })
+// 
+    console.log('AI response',response);
+  }
+   
 
   return (
     <div className="w-full max-w-[95%] sm:max-w-[600px] md:max-w-[800px] lg:max-w-[1024px] xl:max-w-[1200px] 2xl:max-w-[1380px] mx-auto overflow-hidden">
-        {/* <Assignments 
-            assignatureId={parseInt(assignatureId)}
-            area={area}
-        /> */}
-        <Activities />
+        <Input 
+          value={topic}
+          onChange={(e) => setTopic(e.target.value)}
+          placeholder='Enter your prompt here...'
+        />
+        <button className='bg-blue-600 text-slate-50 px-6 py-2' onClick={() => {
+          getAIResponse(topic)
+          setTopic('')
+        }}>Get AI Response</button>
+        {/* <Activities /> */}
     </div>
   )
 }
