2ce7c6250a0bfa4dd641fa91a905ee18d3901e12
podestal <luisrodriguezpodesta@gmail.com>
2025-05-07 07:20:18 -0400

    go back button fix styled

--- src/pages/GradesPage.tsx
+++ src/pages/GradesPage.tsx
@@ -16,10 +16,12 @@ const GradesPage = () => {
   return (
     <div className="w-full max-w-[95%] sm:max-w-[600px] md:max-w-[800px] lg:max-w-[1024px] xl:max-w-[1200px] 2xl:max-w-[1380px] mx-auto px-6 py-12">
       <div className=" grid grid-cols-3 max-lg:hidden">
-      <GoBack 
-          path={`/app/assignatures/${assignatureId}/`}
-          state={{area, assignatureId }}
-        />
+        <div className="flex items-center">
+          <GoBack 
+            path={`/app/assignatures/${assignatureId}/`}
+            state={{area, assignatureId }}
+          />
+        </div>
         <h2 className="text-3xl font-bold text-center mb-2">📊 Calificaciones</h2>
       </div>
       <div className="grid grid-cols-3 lg:hidden ">
